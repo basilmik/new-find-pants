219bab4c136393def96fa328a943dcee49a5eb53
Basil <egsilyamik@gmail.com>
2024-02-16 22:33:26 +0300

    moving item in mouth

--- scripts/krot_body.gd
+++ scripts/krot_body.gd
@@ -1,15 +1,31 @@
 extends CharacterBody2D
 
-var maxSpeed = 50
+var maxSpeed = 100
 var currentRot = 0
 var current_rotated_angle = 0
 var angle_rotate_ad = 0
 var zero_velocity = Vector2()
 
+var item_node
+var nose_marker_node
+
+func _ready():
+	item_node = get_node("../item")
+	nose_marker_node = get_node("nose/CollisionShape2D")
+
+
+
 func _physics_process(delta):
 	
 	get_input()
+	
 	move_and_slide()
+	set_item_position()
+	
+func set_item_position():
+	item_node.rotation = rotation
+	item_node.global_position = nose_marker_node.global_position
+	
 
 func rotate_body():
 	self.rotation = -deg_to_rad(angle_rotate_ad)
